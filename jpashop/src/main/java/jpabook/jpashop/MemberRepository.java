ab1c9134b6e45af5f33df923ee61f9e60cecae4f
홍지민 <baker0929@naver.com>
2023-07-10 22:28:56 +0900

    feat : test Build

--- jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -8,15 +8,18 @@ import javax.persistence.PersistenceContext;
 @Repository
 public class MemberRepository {
     @PersistenceContext
+    // EntityManager 주입
     private EntityManager em;
 
-    public Long save(Member member){
+    // 저장 메소드 생성
+    public Long save(Member member) {
         em.persist(member);
         return member.getId();
+        // 멤버 반환해도 되지 않나?
+        // 커맨드랑 쿼리 분리하는게 원칙임
     }
 
-    public Member find(Long id)
-    {
+    public Member find(Long id) {
         return em.find(Member.class, id);
     }
 }
